df69536e32a9f4f36f0d63f3a76fb460cb692cc8
zml <mustbezml@gmail.com>
2025-01-20 11:11:40 +0300

    fix: changed function to method
    Moved proccessNumericItems function into the obj
    variable to determine this as object's method

--- task3.js
+++ task3.js
@@ -1,20 +1,19 @@
-function proccessNumericItems(obj, returnKeys = false) {
-    let toReturn = (returnKeys) ? [] : 0;
-    for (let key in obj) {
-        if (typeof(obj[key]) == "number") {
-            (returnKeys) ? toReturn.push(key) : toReturn += obj[key];
-        }
-    }
-
-    return (returnKeys) ? toReturn.sort().reverse() : toReturn;
-}
-
 let obj = {
     name: 'Vasya',
     friends: 5,
     likes: 19,
-    projects: 7
+    projects: 7,
+    proccessNumericItems: function(returnKeys = false) {
+        let toReturn = (returnKeys) ? [] : 0;
+        for (let key in this) {
+            if (typeof(this[key]) == "number") {
+                (returnKeys) ? toReturn.push(key) : toReturn += this[key];
+            }
+        }
+    
+        return (returnKeys) ? toReturn.sort().reverse() : toReturn;
+    }
 }
 
-console.log(proccessNumericItems(obj)) // 31
-console.log(proccessNumericItems(obj, true)) // [ 'projects', 'likes', 'friends' ]
+console.log(obj.proccessNumericItems()) // 31
+console.log(obj.proccessNumericItems(true)) // [ 'projects', 'likes', 'friends' ]
